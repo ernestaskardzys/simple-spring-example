c6d0e8393f9de9bf81af99c3ab8a4821a0dcf008
Ernestas Kardzys <ernestas.kardzys@gmail.com>
2016-01-20 14:25:17 +0200

    Commit with all files of the project

---
+++ web/src/main/java/info/ernestas/simple/springexample/web/configs/SimpleWebApplicationInitializer.java
@@ -0,0 +1,23 @@
+package info.ernestas.simple.springexample.web.configs;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class SimpleWebApplicationInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
+        dispatcherContext.register(SimpleMvcConfiguration.class);
+
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(dispatcherContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+
+}
