c8aa788f6dd292a1ca246324b413a9787c30c58b
Ernestas Kardzys <ernestas.kardzys@gmail.com>
2016-02-02 16:24:37 +0200

    Fixed configuration for Eclipse

--- core/src/main/java/info/ernestas/simple/springexample/core/service/NameService.java
+++ core/src/main/java/info/ernestas/simple/springexample/core/service/NameService.java
@@ -3,11 +3,13 @@ package info.ernestas.simple.springexample.core.service;
 import info.ernestas.simple.springexample.core.dao.NameDao;
 import info.ernestas.simple.springexample.core.model.Name;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Repository
+@Service
+@Transactional
 public class NameService {
 
     @Autowired
