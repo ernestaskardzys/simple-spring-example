be9d764457e2e79699c112fab4b4d3373bdbad34
Ernestas Kardzys <ernestas.kardzys@gmail.com>
2016-02-19 21:21:54 +0200

    Refactored services to be implementations of interfaces

--- core/src/main/java/info/ernestas/simple/springexample/core/dao/NameDao.java
+++ core/src/main/java/info/ernestas/simple/springexample/core/dao/NameDao.java
@@ -1,26 +1,13 @@
 package info.ernestas.simple.springexample.core.dao;
 
-import info.ernestas.simple.springexample.core.dao.repository.NameRepository;
 import info.ernestas.simple.springexample.core.model.Name;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-public class NameDao {
+public interface NameDao {
 
-    @Autowired
-    private NameRepository nameRepository;
+    List<Name> findAll();
 
-    @Cacheable("namesCache")
-    public List<Name> findAll() {
-        return nameRepository.findAll();
-    }
-
-    public Name findByIdAndName(Integer id, String name) {
-        return nameRepository.findByIdAndName(id, name);
-    }
+    Name findByIdAndName(Integer id, String name);
 
 }
