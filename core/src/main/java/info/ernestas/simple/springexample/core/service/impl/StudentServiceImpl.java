5ead9f0c12e99e98d334c4279dd91b11d3c37562
Ernestas Kardzys <ernestas.kardzys@gmail.com>
2016-02-23 13:54:36 +0200

    Code cleanup

--- core/src/main/java/info/ernestas/simple/springexample/core/service/impl/StudentServiceImpl.java
+++ core/src/main/java/info/ernestas/simple/springexample/core/service/impl/StudentServiceImpl.java
@@ -1,15 +1,15 @@
 package info.ernestas.simple.springexample.core.service.impl;
 
 import info.ernestas.simple.springexample.core.dao.StudentDao;
-import info.ernestas.simple.springexample.core.dao.repository.StudentRepository;
 import info.ernestas.simple.springexample.core.model.Student;
+import info.ernestas.simple.springexample.core.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class StudentServiceImpl implements info.ernestas.simple.springexample.core.service.StudentService {
+public class StudentServiceImpl implements StudentService {
 
     @Autowired
     private StudentDao studentDao;
